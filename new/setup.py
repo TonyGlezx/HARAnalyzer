a4e101e2da5941b6a959bc2b1eb562a2a994ad18
Tony Gonzalez <tony@debian-BULLSEYE-live-builder-AMD64>
2023-05-27 16:53:29 -0600

    First commit

---
+++ new/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='HARAnalyzer',
+    version='0.1',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='Tony Gonzalez',
+    author_email='economiaug@gmail.com',
+    description='A utility for analyzing HAR files',
+    install_requires=[
+        # List of dependencies
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
